90411ad465ffe74799ef405fe73468bc6ccf1bd8
okwave-mi-suzuki <mtr.suzuki@gmail.com>
2021-03-08 08:09:07 +0900

    homeにタイトルとファイルアップロード部分を追加（アップロード部分は見た目だけ）

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
+    @ViewChild('fileInput', {static: true}) fileInput;
+    file: File | null = null;
 
-}
+    onClickFileInputButton(): void {
+        this.fileInput.nativeElement.click();
+    }
+
+    onChangeFileInput(): void {
+        const files: { [key: string]: File } = this.fileInput.nativeElement.files;
+        this.file = files[0];
+    }}
