dd654bce930142d965ff853b70ee2b4fa7d151bc
Mitsuru Suzuki <mtr.suzuki@gmail.com>
2022-01-03 08:36:15 +0900

    prod以外ではdemo.jsonを自動で読み込むようにする

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AbstractComponent } from 'src/app/common/components/base/abstract.component';
+import {environment} from '../environments/environment';
+import {SubjectManager} from './common/services/sabject-manager/subject.manager';
+import {ImportService} from './common/services/import/import.service';
 
 @Component({
     selector: 'app-root',
@@ -7,11 +10,19 @@ import { AbstractComponent } from 'src/app/common/components/base/abstract.compo
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent extends AbstractComponent {
-    constructor() {
+    constructor(private importService: ImportService, private subjectManager: SubjectManager) {
         super();
     }
 
     ngOnInit(): void {
         super.ngOnInit();
+        if (!environment.production) {
+            this.subscriptions.push(
+                this.importService.importSample().subscribe(e => {
+                    this.importService.next(e);
+                    this.subjectManager.importFileName.next('demo.json');
+                }),
+            );
+        }
     }
 }
